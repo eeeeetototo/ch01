a46f1d40d35fbcf915aadd9c7b56df166b192039
eeeeetototo <147032525+eeeeetototo@users.noreply.github.com>
2023-11-18 23:09:17 +0900

    1

---
+++ lib/NewProject.dart
@@ -0,0 +1,13 @@
+class NewProject extends StatefulWidget {
+  const NewProject({super.key});
+
+  @override
+  State<NewProject> createState() => _NewProjectState();
+}
+
+class _NewProjectState extends State<NewProject> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
